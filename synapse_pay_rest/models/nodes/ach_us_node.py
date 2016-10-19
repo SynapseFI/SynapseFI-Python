9163eca58db510d40bfcccc4dde8cd42be27d169
stevula <brodericksteven@gmail.com>
2016-10-19 15:20:56 -0700

    Add flow for verifying ach us node after bank login

--- synapse_pay_rest/models/nodes/ach_us_node.py
+++ synapse_pay_rest/models/nodes/ach_us_node.py
@@ -1,4 +1,5 @@
 from .base_node import BaseNode
+# from .unverified_node import UnverifiedNode
 
 
 class AchUsNode(BaseNode):
@@ -6,15 +7,28 @@ class AchUsNode(BaseNode):
     """
 
     @classmethod
+    def create_unverified_node(cls, user, response):
+        return cls(user=user,
+                   mfa_access_token=response['mfa']['access_token'],
+                   mfa_message=response['mfa']['message'],
+                   mfa_verified=False)
+
+    @classmethod
     def create_via_bank_login(cls, user=None, bank_name=None, username=None,
                               password=None):
         payload = super().payload_for_create('ACH-US',
                                              bank_name=bank_name,
                                              username=username,
-                                             password=password)
+                                             password=password,
+                                             mfa_verified=True)
         user.authenticate()
         response = user.client.nodes.create(user.id, payload)
-        return cls.multiple_from_response(user, response['nodes'])
+        if 'mfa' in response:
+            # create unverified node
+            return cls.create_unverified_node(user, response)
+        else:
+            # no mfa needed
+            return cls.multiple_from_response(user, response['nodes'])
 
     @classmethod
     def payload_for_create(cls, nickname, account_number, routing_number,
@@ -32,3 +46,16 @@ class AchUsNode(BaseNode):
         payload = {'micro': [amount1, amount2]}
         response = self.user.client.nodes.update(self.user.id, self.id, payload)
         return self.from_response(self.user, response)
+
+    def answer_mfa(self, answer):
+        payload = {'access_token': self.mfa_access_token, 'mfa_answer': answer}
+        response = self.user.client.nodes.create(self.user.id, payload)
+
+        if response['error_code'] == '0':
+            # correct answer
+            self.mfa_verified = True
+            return self.multiple_from_response(self.user, response['nodes'])
+        elif response['error_code'] == '10':
+            self.mfa_access_token = response['mfa']['access_token']
+            self.mfa_message = response['mfa']['message']
+            return self

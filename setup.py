a77b4bcd41cce8240b07ee1e4e5c5ba92a83bf26
easak <easak@synapsefi.com>
2018-10-23 12:20:11 -0700

    Update versioning

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='3.3.1',
+    version='3.4.0',
 
     description='SynapsePay Rest Native Python Library',
 

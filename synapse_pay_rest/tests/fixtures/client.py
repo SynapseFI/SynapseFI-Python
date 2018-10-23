77a53215835dc30a907f8bd8f582319b18d246b4
easak <easak@synapsefi.com>
2018-10-23 11:53:04 -0700

    Final clean up for institutions and statements

--- synapse_pay_rest/tests/fixtures/client.py
+++ synapse_pay_rest/tests/fixtures/client.py
@@ -2,12 +2,6 @@ import os
 import sys
 from synapse_pay_rest.client import Client
 
-# try:
-#   CLIENT_ID = os.environ['TEST_CLIENT_ID']
-#   CLIENT_SECRET = os.environ['TEST_CLIENT_SECRET']
-# except KeyError:
-#   CLIENT_ID = input("Please enter client ID: ")
-#   CLIENT_SECRET = input("Please enter client secret: ")
 if sys.version_info[0] >= 3:
   try:
     CLIENT_ID = os.environ['TEST_CLIENT_ID']
@@ -23,8 +17,7 @@ else:
       CLIENT_ID = raw_input("Please enter client ID: ")
       CLIENT_SECRET = raw_input("Please enter client secret: ")
 
-# FINGERPRINT = '737321631015eefd8dd6573ccce33319'
-FINGERPRINT = 'c7e516361e6e5453de96223616d73a7b'
+FINGERPRINT = 'INPUT_USER_FINGERPRINT_HERE'
 
 IP_ADDRESS = '127.0.0.1'
 

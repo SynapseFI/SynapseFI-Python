9edbe941a60580ac178a1a4d59bcb9d9bdada4c7
Easak Hong <easakhong@Easaks-MacBook-Pro.local>
2019-03-22 16:25:01 -0700

    Fix version number in setup file

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='3.4.6',
+    version='3.4.7',
 
     description='SynapsePay Rest Native Python Library',
 
